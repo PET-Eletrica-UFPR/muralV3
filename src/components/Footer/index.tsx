67bd04cf2dca60a8165398f6267efab8ea4b0591
Jose <Jose>
2022-05-31 13:31:14 -0300

    Footer com horário de intercampi terminados

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,7 @@
 import { Container, Content } from "./styles"
 import Clock from 'react-live-clock'
+import Marquee from "react-fast-marquee";
+
 
 import 'react-clock/dist/Clock.css';
 
@@ -10,8 +12,12 @@ export function Footer() {
         <Container>
             <Content>
                     <marquee style={{ color: 'black', fontSize: '1.5em'}}>
-                        Horários Intercampi (saída do Politécnico):  
-                        07:35, 08:30, 10:40, 11:35, 11:40
+                        <strong> Horários Intercampi 01 (saída do Politécnico): </strong>
+                        07:30, 08:30, 10:40, 11:35, 11:40, 13:20, 15:50, 18:35<strong> • </strong>
+                        <strong> Horários Intercampi 02 (saída do Politécnico): </strong>
+                        07:35, 10:40, 11:40, 13:15, 15:10, 17:40, 15:50, 18:35<strong> • </strong>
+                        <strong> Horários Intercampi 04 (saída do Politécnico): </strong>
+                        08:25, 10:40, 11:30, 15:10, 17:40<strong> • </strong>
                     </marquee>
                 
                 <div>
