a8bdaaa9f73620755ac9b28275be9190e74a12a3
Jose <Jose>
2022-05-31 14:19:41 -0300

    Marquee usando componente

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,14 +11,14 @@ export function Footer() {
     return (
         <Container>
             <Content>
-                    <marquee style={{ color: 'black', fontSize: '1.5em'}}>
-                        <strong> Horários Intercampi 01 (saída do Politécnico): </strong>
-                        07:30, 08:30, 10:40, 11:35, 11:40, 13:20, 15:50, 18:35<strong> • </strong>
-                        <strong> Horários Intercampi 02 (saída do Politécnico): </strong>
-                        07:35, 10:40, 11:40, 13:15, 15:10, 17:40, 15:50, 18:35<strong> • </strong>
-                        <strong> Horários Intercampi 04 (saída do Politécnico): </strong>
-                        08:25, 10:40, 11:30, 15:10, 17:40
-                    </marquee>
+                    <Marquee gradient= {false} style={{ padding: 0, fontSize: 2,}}>
+                       Horários Intercampi 01 (saída do Politécnico): &nbsp;
+                        07:30, &nbsp;08:30, &nbsp;10:40, &nbsp;11:35, &nbsp;11:40, &nbsp;13:20, &nbsp;15:50, &nbsp;18:35 &nbsp; • &nbsp;   
+                         Horários Intercampi 02 (saída do Politécnico): &nbsp;
+                        07:35, &nbsp;10:40, &nbsp;11:40, &nbsp;13:15, &nbsp;15:10, &nbsp;17:40, &nbsp;15:50, &nbsp;18:35 &nbsp; • &nbsp; 
+                        Horários Intercampi 04 (saída do Politécnico): &nbsp;
+                        08:25, &nbsp;10:40, &nbsp;11:30, &nbsp;15:10, &nbsp;17:40
+                    </Marquee>
                 
                 <div>
                     <Clock format={'HH:mm'} ticking={true} timezone={'America/Sao_Paulo'} />
