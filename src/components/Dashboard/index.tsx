f4d27ab6d9418db6146670a080da38d448d44e64
José Eduardo <joseduardosilva.42@gmail.com>
2022-06-03 10:46:07 -0300

    Retirada a imagem do logo do PET no teste do Dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -39,7 +39,6 @@ export function Dashboard() {
           className="mySwiper"
         >
           <SwiperSlide><h1>{data}</h1> <p>{abstract}</p></SwiperSlide>
-          <SwiperSlide><img src="/logo.jpeg" alt="qqchose" width={10} /></SwiperSlide>
         </Container>
 
       </>
