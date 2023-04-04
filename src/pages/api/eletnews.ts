12b5242e87b79716e38107fddd2a5c17f0629d6a
Rickmeira <meiraricardo@hotmail.com>
2023-04-04 15:43:34 -0300

    news2

--- src/pages/api/eletnews.ts
+++ src/pages/api/eletnews.ts
@@ -1,13 +1,12 @@
 import * as cheerio from "cheerio"
 
+
 export default async function handler(req, res) {
-  const response = await fetch(`https://ufpr.br/mais-noticias/`)
-  const htmlString = await response.text()
-  const $ = cheerio.load(htmlString)
 
-  // seleciona o segundo título e resumo na lista
-  const title2 = $('div.uc_post_list_title').eq(1).text()
-  const abstract2 = $('div.uc_post_content').eq(1).text()
-
-  res.status(200).json({ title2, abstract2 })
-}
+    const response = await fetch(`https://ufpr.br/mais-noticias/`)
+    const htmlString = await response.text()
+    const $ = cheerio.load(htmlString)
+    const title = $('div.uc_post_list_title').eq(1).text()
+    const abstract = $('div.uc_post_content').eq(1).text()
+    res.status(200).json({ title, abstract })
+  }
