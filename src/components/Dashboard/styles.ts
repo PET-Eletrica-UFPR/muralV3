4105cce25ab91939893a1afcad68e65242b96405
Rickmeira <meiraricardo@hotmail.com>
2023-10-30 21:46:42 -0300

    halloween

--- src/components/Dashboard/styles.ts
+++ src/components/Dashboard/styles.ts
@@ -2,21 +2,21 @@ import styled from "styled-components";
 import { Swiper, SwiperSlide } from 'swiper/react'
 
 export const Container = styled(Swiper) `
-    background: black;
+    background: orange;
     height: calc(100vh - 15.96rem);
 
     h1 {
         padding: 1rem 2rem 2rem 2rem;
         font-size: 4rem;
         font-weight: bolder;
-        color: #ffd230;
+        color: #000000;
     }
 
     p {
         padding: 0 2rem 2rem 2rem;
         margin-top: 3.5rem;
         font-size: 3.5rem;
-        color: white;
+        color: #FF0000;
     }
 
     img {
