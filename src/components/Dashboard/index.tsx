596915c19dbdafe5117b9afe98f650d75c188064
PET-UFPR-Eletrica <93662762+PET-UFPR-Eletrica@users.noreply.github.com>
2024-10-10 13:06:24 -0300

    crase

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -47,13 +47,13 @@ https://github.com/PET-Eletrica-UFPR/muralV3/blob/main/src/components/Dashboard/
         modules={[Autoplay]}
         className="mySwiper"
       >
-        <!-- <SwiperSlide><h1>{titleUfprnews}</h1><p>{abstractUfprnews}</p></SwiperSlide>
-        <SwiperSlide><h1>{titleEd}</h1><p>{abstractEd}</p></SwiperSlide> -->
+         <SwiperSlide><h1>{titleUfprnews}</h1><p>{abstractUfprnews}</p></SwiperSlide>
+        <SwiperSlide><h1>{titleEd}</h1><p>{abstractEd}</p></SwiperSlide>
         <SwiperSlide><img src="/fablab.jpg" alt="fablab" height="100%" /></SwiperSlide>
         <SwiperSlide><img src="/redess.jpg" alt="redess" height="100%" /></SwiperSlide>
         <SwiperSlide><img src="/QRCODE.png" alt="qrcode" height="100%" /></SwiperSlide>
         <SwiperSlide><img src="/forms.jpeg" alt="forms" height="100%" /></SwiperSlide>
-        <SwiperSlide><img src="/muralv3.jpg" alt="descarte de lixo eletronico" height="100%" /></SwiperSlide>
+        <SwiperSlide><img src="/muralv33.jpg" alt="descarte de lixo eletronico" height="100%" /></SwiperSlide>
        
 
       </Container>
