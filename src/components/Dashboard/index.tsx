4d7a35839bf4f416afb232d512ecc3cbab61dd68
PET-UFPR-Eletrica <93662762+PET-UFPR-Eletrica@users.noreply.github.com>
2025-03-07 16:27:18 -0300

    Update index.tsx

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -52,7 +52,7 @@ https://github.com/PET-Eletrica-UFPR/muralV3/blob/main/src/components/Dashboard/
         <SwiperSlide><img src="/redess.jpg" alt="redess" height="100%" /></SwiperSlide>
         <SwiperSlide><img src="/QRCODE.png" alt="qrcode" height="100%" /></SwiperSlide>
         <SwiperSlide><img src="/forms.jpeg" alt="forms" height="100%" /></SwiperSlide>
-        <SwiperSlide><img src="/muralv33.jpg" alt="descarte de lixo eletronico" height="100%" /></SwiperSlide>
+      
       
        
        
